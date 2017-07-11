11ae63bfd6cedf3a1aa337c31dba844d4e24da3f
JustFlare <just1flare@gmail.com>
2017-07-12 00:34:42 +0300

    test bot to check webhook work

---
+++ nikon_bot.py
@@ -0,0 +1,56 @@
+import telebot
+from .conf import *
+import flask
+import logging
+
+logger = telebot.logger
+telebot.logger.setLevel(logging.INFO)
+
+WEBHOOK_URL_BASE = "https://{}:{}".format(WEBHOOK_HOST, WEBHOOK_PORT)
+WEBHOOK_URL_PATH = "/{}/".format(TOKEN)
+
+START_MESSAGE = '''Здравствуйте! 
+Этот бот умеет делать чудесные вещи, например подсчитывать число символов в вашем сообщении'''
+
+bot = telebot.TeleBot(TOKEN)
+bot.remove_webhook()  # удаляем предыдущие вебхуки, если они были
+bot.set_webhook(url=WEBHOOK_URL_BASE+WEBHOOK_URL_PATH)  # добавляем новый вебхук
+
+app = flask.Flask(__name__)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.send_message(message.chat.id, START_MESSAGE)
+
+
+# TODO: for test
+@bot.message_handler(func=lambda m: True)  # этот обработчик реагирует все прочие сообщения
+def send_len(message):
+    bot.send_message(message.chat.id,
+                     'В вашем сообщении {} символов.'.format(len(message.text)))
+
+
+# TODO: check if it is necessary
+# пустая главная страничка для проверки
+@app.route('/', methods=['GET', 'HEAD'])
+def index():
+    return 'ok'
+
+
+# обрабатываем вызовы вебхука
+@app.route(WEBHOOK_URL_PATH, methods=['POST'])
+def webhook():
+    if flask.request.headers.get('content-type') == 'application/json':
+        json_string = flask.request.get_data().decode('utf-8')
+        update = telebot.types.Update.de_json(json_string)
+        bot.process_new_updates([update])
+        return ''
+    else:
+        flask.abort(403)
+
+# Start flask server
+app.run(host=WEBHOOK_LISTEN,
+        port=WEBHOOK_PORT,
+        ssl_context=(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV),
+        debug=True)
