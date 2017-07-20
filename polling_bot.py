834def93ed9c6433827d963908673bab69ba673d
JustFlare <just1flare@gmail.com>
2017-07-20 13:08:41 +0300

    Refactoring: add phrases module, divide help and start commands

--- polling_bot.py
+++ polling_bot.py
@@ -1,47 +1,47 @@
 import MySQLdb
 import telebot
 from telebot import types
-from conf import *
+import conf
+import phrases
 import logging
 from random import shuffle
 
-START_MESSAGE = '''Здравствуйте! 
-Этот бот призван помочь начинающим фотографам и облегчить взаимодействие с фотоаппаратами Nikon
-Содержание:
-/question - ответы на вопрос
-/photo - поиск фотографий по различным категориям
-/articles - статьи 
-/guides - обучающие материалы
-/buy - где купить
-'''
 
 photo_search_categories = ["Экспозиция", "Объектив", "Жанр", "Камера", "Автор"]
 
-hide_board = types.ReplyKeyboardRemove()
+hide = types.ReplyKeyboardRemove()
 return_button = types.InlineKeyboardButton("Меню")
+
 search_photo_keyboard = types.ReplyKeyboardMarkup(one_time_keyboard=False)
 buttons = [types.InlineKeyboardButton(c) for c in photo_search_categories]
 search_photo_keyboard.add(buttons[0], buttons[1], buttons[2])
 search_photo_keyboard.add(buttons[3], buttons[4], return_button)
 
+menu = types.ReplyKeyboardMarkup()
+menu.add(types.InlineKeyboardButton)
+
 logger = telebot.logger
 telebot.logger.setLevel(logging.INFO)
 
-
-bot = telebot.TeleBot(TOKEN)
+bot = telebot.TeleBot(conf.TOKEN)
 bot.remove_webhook()
 
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, START_MESSAGE)
+    bot.send_message(message.chat.id, phrases.start_message)
+
+
+@bot.message_handler(commands=['help'])
+def start(message):
+    bot.send_message(message.chat.id, phrases.help_message)
 
 
 @bot.message_handler(commands=['question'])
 def search_faq(message):
     keyboard = types.InlineKeyboardMarkup()
     url_button = types.InlineKeyboardButton(text="Перейти на сайт",
-                                            url=FAQ_SEARCH_LINK)
+                                            url=conf.FAQ_SEARCH_LINK)
     keyboard.add(url_button)
     bot.send_message(message.chat.id,
                      "Попробуйте найти ответы с помощью функции поиска на нашем сайте",
@@ -129,12 +129,12 @@ def search_by_author(message):
 def show_photos(message, data, not_found_text):
     if len(data) == 0:
         bot.send_message(message.chat.id, not_found_text, reply_markup=search_photo_keyboard)
-    elif len(data) < MAX_PHOTOS_TO_SHOW:
+    elif len(data) < conf.MAX_PHOTOS_TO_SHOW:
         for row in data:
             print_photo(message, row)
     else:
         shuffle(data)
-        for row in data[:MAX_PHOTOS_TO_SHOW]:
+        for row in data[:conf.MAX_PHOTOS_TO_SHOW]:
             print_photo(message, row)
         # TODO: кнопка/опция "показать еще"
 
@@ -158,14 +158,14 @@ def buy(message):
 def buy(message):
     keyboard = types.InlineKeyboardMarkup()
     url_button = types.InlineKeyboardButton(text="Перейти в магазин",
-                                            url=STORE_LINK)
+                                            url=conf.STORE_LINK)
     keyboard.add(url_button)
     bot.send_message(message.chat.id, "Где купить", reply_markup=keyboard)
 
 
 if __name__ == "__main__":
     print('start bot')
-    connection = MySQLdb.connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME,
+    connection = MySQLdb.connect(conf.DB_HOST, conf.DB_USER, conf.DB_PASSWORD, conf.DB_NAME,
                                  use_unicode=True, charset="utf8")
     cursor = connection.cursor()
 
