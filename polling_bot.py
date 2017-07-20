10ff559fd38cdf603a94785705254c62ea5739fb
JustFlare <just1flare@gmail.com>
2017-07-20 12:48:06 +0300

    Replace InlineKeyboard with ReplyKeaboard in photo search

--- polling_bot.py
+++ polling_bot.py
@@ -6,7 +6,7 @@ import logging
 from random import shuffle
 
 START_MESSAGE = '''Здравствуйте! 
-Этот бот призван помочь начинающим фотографам и облегчить взаимодействие с фотоаппаратами nikon
+Этот бот призван помочь начинающим фотографам и облегчить взаимодействие с фотоаппаратами Nikon
 Содержание:
 /question - ответы на вопрос
 /photo - поиск фотографий по различным категориям
@@ -15,8 +15,14 @@ START_MESSAGE = '''Здравствуйте!
 /buy - где купить
 '''
 
-photo_search_categories = {"exposure": "Экспозиция", "lens": "Объектив",
-                           "genre": "Жанр", "camera": "Камера", "author": "Автор"}
+photo_search_categories = ["Экспозиция", "Объектив", "Жанр", "Камера", "Автор"]
+
+hide_board = types.ReplyKeyboardRemove()
+return_button = types.InlineKeyboardButton("Меню")
+search_photo_keyboard = types.ReplyKeyboardMarkup(one_time_keyboard=False)
+buttons = [types.InlineKeyboardButton(c) for c in photo_search_categories]
+search_photo_keyboard.add(buttons[0], buttons[1], buttons[2])
+search_photo_keyboard.add(buttons[3], buttons[4], return_button)
 
 logger = telebot.logger
 telebot.logger.setLevel(logging.INFO)
@@ -46,42 +52,57 @@ def search_faq(message):
 
 @bot.message_handler(commands=['photo'])
 def search_photo(message):
-    keyboard = types.InlineKeyboardMarkup()
-    for k, v in photo_search_categories.items():
-        keyboard.add(types.InlineKeyboardButton(text=v, callback_data=k))
-
-    bot.send_message(message.chat.id, "Выберите категорию поиска:", reply_markup=keyboard)
+    bot.send_message(message.chat.id, "Выберите категорию поиска:",
+                     reply_markup=search_photo_keyboard)
 
 
-@bot.callback_query_handler(func=lambda call: call.data in photo_search_categories)
-def search_category_choice(call):
+@bot.message_handler(func=lambda message: message.text in photo_search_categories)
+def search_category_choice(message):
     markup = types.ForceReply(selective=False)
-    if call.data == 'exposure':
-        msg = bot.send_message(call.message.chat.id,
+    if message.text == 'Экспозиция':
+        msg = bot.send_message(message.chat.id,
                                "Введите значение экспозиции (например, 1/125 или 1/2500)",
                                reply_markup=markup)
         bot.register_next_step_handler(msg, search_by_exposure)
-    elif call.data == 'lens':
-        msg = bot.send_message(call.message.chat.id,
+    elif message.text == 'Объектив':
+        msg = bot.send_message(message.chat.id,
                                "Введите название (можно частичное) объектива",
                                reply_markup=markup)
         bot.register_next_step_handler(msg, search_by_lens)
-    elif call.data == 'genre':
+    elif message.text == 'Жанр':
         keyboard = types.InlineKeyboardMarkup()
         for g in photo_genres:
             keyboard.add(types.InlineKeyboardButton(text=g, callback_data=g))
 
-        bot.send_message(call.message.chat.id, "Выберите жанр:", reply_markup=keyboard)
-    elif call.data == 'camera':
-        msg = bot.send_message(call.message.chat.id,
+        bot.send_message(message.chat.id, "Выберите жанр:", reply_markup=keyboard)
+    elif message.text == 'Камера':
+        msg = bot.send_message(message.chat.id,
                                "Введите название камеры (например Nikon D4)",
                                reply_markup=markup)
         bot.register_next_step_handler(msg, search_by_camera)
-    elif call.data == 'author':
-        msg = bot.send_message(call.message.chat.id,
+    elif message.text == 'Автор':
+        msg = bot.send_message(message.chat.id,
                                "Введите имя и/или фамилию автора",
                                reply_markup=markup)
         bot.register_next_step_handler(msg, search_by_author)
+    else:
+        bot.send_message(message.chat.id, "Неверное название категории. "
+                                          "Выберите одну из предложенных на клавиатуре.")
+
+
+def search_by_exposure(message):
+    query = "select * from photos where {0} = \'{1}\'".format("exposure", message.text)
+    cursor.execute(query)
+    # logging.info(query)
+    show_photos(message, list(cursor.fetchall()),
+                "фотографий с заданной экспозицией не найдено")
+
+
+def search_by_lens(message):
+    query = "select * from photos where {0} LIKE \'%{1}%\'".format("lens", message.text)
+    cursor.execute(query)
+    # logging.info(query)
+    show_photos(message, list(cursor.fetchall()), "фотографий с таким объективом не найдено")
 
 
 @bot.callback_query_handler(func=lambda call: call.data in photo_genres)
@@ -91,34 +112,23 @@ def search_category_choice(call):
                 "фотографий выбранного жанра не найдено")
 
 
-def search_by_exposure(message):
-    cursor.execute("select * from photos where {0} = \'{1}\'".format("exposure", message.text))
-    show_photos(message, list(cursor.fetchall()),
-                "фотографий с заданной экспозицией не найдено")
-
-
-def search_by_lens(message):
-    cursor.execute(
-        "select * from photos where {0} LIKE \'%{1}%\'".format("lens", message.text))
-    show_photos(message, list(cursor.fetchall()), "фотографий с таким объективом не найдено")
-
-
 def search_by_camera(message):
-    cursor.execute(
-        "select * from photos where {0} = \'{1}\'".format("camera", message.text))
+    query = "select * from photos where {0} = \'{1}\'".format("camera", message.text)
+    cursor.execute(query)
+    # logging.info(query)
     show_photos(message, list(cursor.fetchall()), "фотографий с заданной камерой не найдено")
 
 
 def search_by_author(message):
-    cursor.execute(
-        "select * from photos where {0} LIKE \'%{1}%\'".format("author", message.text))
+    query = "select * from photos where {0} LIKE \'%{1}%\'".format("author", message.text)
+    cursor.execute(query)
+    # logging.info(query)
     show_photos(message, list(cursor.fetchall()), "такого автора пока нет в нашей базе данных")
 
 
 def show_photos(message, data, not_found_text):
     if len(data) == 0:
-        bot.send_message(message.chat.id, not_found_text)
-        # TODO: предложить пользователю попробовать еще раз
+        bot.send_message(message.chat.id, not_found_text, reply_markup=search_photo_keyboard)
     elif len(data) < MAX_PHOTOS_TO_SHOW:
         for row in data:
             print_photo(message, row)
@@ -133,7 +143,8 @@ def print_photo(message, row):
     link, exposure, lens, genre, aperture, camera, iso, focal_length, author = row
     bot.send_message(message.chat.id, "{0}\n{1}\n{2}\n{3}\n{4}\n{5}\n{6}\n{7}\n{8}"
                      .format(link, exposure, lens, genre, aperture, camera, iso,
-                             focal_length, author))
+                             focal_length, author),
+                     reply_markup=search_photo_keyboard)
 
 ############################################################################
 
