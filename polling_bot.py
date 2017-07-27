54bb3b77f14912b5ab9ca04575e3e60ea750934a
JustFlare <just1flare@gmail.com>
2017-07-27 12:06:58 +0300

    add search by articles

--- polling_bot.py
+++ polling_bot.py
@@ -156,7 +156,7 @@ def search_by_lens(call):
 
 
 @bot.callback_query_handler(func=lambda call: call.data in photo_genres)
-def search_category_choice(call):
+def search_genres(call):
     cursor.execute("select * from photos where {0} = \'{1}\'".format("genre", call.data))
     show_photos(call.message, list(cursor.fetchall()),
                 "фотографий выбранного жанра не найдено")
@@ -204,7 +204,22 @@ def print_photo(message, row):
 
 @bot.message_handler(regexp="(\/articles)|(Статьи)")
 def articles_search(message):
-    bot.send_message(message.chat.id, "Функция находится в разработке")
+    keyboard = types.InlineKeyboardMarkup()
+    for g in article_genres:
+        keyboard.add(types.InlineKeyboardButton(text=g, callback_data=g))
+    bot.send_message(message.chat.id, "Выберите жанр:", reply_markup=keyboard)
+
+
+@bot.callback_query_handler(func=lambda call: call.data in article_genres)
+def get_articles(call):
+    cursor.execute("select * from articles where {0} = \'{1}\'".format("genre", call.data))
+    res = list(cursor.fetchall())
+    if res:
+        for row in res:
+            bot.send_message(call.message.chat.id,
+                             "{0}\n{1}\n{2}".format(row[1], row[2], row[0]))
+    else:
+        bot.send_message(call.message.chat.id, "Статей выбранного жанра не найдено")
 
 ############################################################################
 
@@ -282,6 +297,9 @@ if __name__ == "__main__":
 
     cursor.execute('select distinct(category) from infographics')
     guides_categories = [c[0] for c in list(cursor.fetchall())]
+
+    cursor.execute('select distinct(genre) from articles')
+    article_genres = [g[0] for g in list(cursor.fetchall())]
 
     bot.polling(none_stop=True)
     cursor.close()
